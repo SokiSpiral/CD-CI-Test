62b9dd07ccde1b95eb080683b5ba59ad0dc4d3ff
ss-kitamura <kitamura@spiralsense.jp>
2025-02-12 13:00:25 +0900

    WebhookのHandlerのベースを作成

---
+++ WebhookHandler.py
@@ -0,0 +1,11 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class WebhookHandler (BaseHTTPRequestHandler):
+    def do_POST(self):
+        return
+
+if __name__ == "__main__":
+    server_address = ("0.0.0.0", 8000)
+    httpd = HTTPServer(server_address, WebhookHandler)
+    print("Webhook Listening on port 8000...")
+    httpd.serve_forever();
